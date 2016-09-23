0dfbf0c26c9e92facb9e4922e5c2909c99bd6035
James Tautges <jamesdtautges@gmail.com>
2016-09-23 19:43:53 +0100

    Skeleton of Store API

---
+++ Store.py
@@ -0,0 +1,15 @@
+class Store:
+    def hasPermission(user, label, transactionType):
+        return
+
+    def applyTransaction(transaction):
+        return
+
+    def checkPassword(user, password):
+        return
+
+    def userExists(user):
+        return
+
+    def getField(label):
+        return
