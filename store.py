70c7344704ea71346253e853a3419fb96770cd19
James Tautges <jamesdtautges@gmail.com>
2016-09-24 10:27:23 +0100

    Changed some stuff to leverage flat data structure and increased generality for x.y cases

--- store.py
+++ store.py
@@ -6,6 +6,7 @@ class Store:
         self.users = []
 
     def has_permission(self, username, label, transactionType):
+        label = label.split()[0]
         userId = -1
         for index, user in enumerate(self.users):
             if user.name == username:
@@ -32,17 +33,14 @@ class Store:
 
     def field_exists(self, field):
         tags = field.split('.')
-        field = self.fields
-        for tag in tags:
-            if tag not in field.keys():
-                return False
-
-        return True
+        if len(tags) == 1:
+            return tags[0] in self.fields.keys()
+        else:
+            return tags[0] in self.fields.keys() and tags[1] in self.fields[tags[0]]["value"]
 
     def get_field(self, label):
-        def get_field(label):
-            labellist = label.split('.')
-            if len(labellist) == 1:
-                return self.fields[labellist[0]]["value"]
-            elif len(labellist) == 2:
-                return self.fields[labellist[0]]["value"][labellist[1]]
+        labellist = label.split('.')
+        if len(labellist) == 1:
+            return self.fields[labellist[0]]["value"]
+        elif len(labellist) == 2:
+            return self.fields[labellist[0]]["value"][labellist[1]]
