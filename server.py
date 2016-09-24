9de0c309f90ce86b2e4fe61c937707a59a775ed5
Lukas Cerny <cerny.lukas.oto@gmail.com>
2016-09-24 23:17:29 +0100

    Syntax errors.

--- server.py
+++ server.py
@@ -7,10 +7,10 @@ class Server(object):
     def __init__(self, hostname, port):
         self.address = (hostname, port)
 
-    def start(self):
+    def start(self, interpreter):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind(self.address)
-        sock.listen(1)
+        self.sock.listen(1)
         while True:
             self.connection, client_address = self.sock.accept()    
             try:
