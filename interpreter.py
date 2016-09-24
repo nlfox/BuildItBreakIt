6289ca7870848037b85e20a197f72e04ee32f1b3
Matthew Dupraz <matthew.dupraz@gmail.com>
2016-09-24 08:43:49 +0000

    Interpreter docstrings

--- interpreter.py
+++ interpreter.py
@@ -16,8 +16,8 @@ class _TokenParser(object):
     def __init__(self, tokens):
         self.tokens = tokens
 
-    # Removes and returns first element and throws error if types don't match
     def expect(self, *args):
+        '''Removes and returns first element, or throws an error if types do not match'''
         next_token = self.next()
         if next_token == None:
             raise ValueError("Unexpected end of input")
@@ -25,20 +25,21 @@ class _TokenParser(object):
             raise ValueError("Unexpected token")
         return next_token
     
-    # Pemoves and returns first element or None if empty
     def next(self):
+        '''Removes and returns first element or None if queue empty'''
         try:
             return self.tokens.pop(0)
         except IndexError:
             return None
 
 class Interpreter(object):
-    # custom init command to be called after actual initialization 
     def init(self, store, server):
+        '''Custom init command to be called after the actual initialization'''
         self.store = store
         self.server = server
 
     def accept_tokens(self, tokens):
+        '''Accepts a list of LexTokens and interprets it to perform actions on the store and server'''
         parser = _TokenParser(tokens)
         
         try:
