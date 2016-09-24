4daf218b784fc450f01306063a4bdd27c603ebe6
James Tautges <jamesdtautges@gmail.com>
2016-09-24 11:09:40 +0100

    Wrote principal management commands

--- store.py
+++ store.py
@@ -3,32 +3,52 @@
 class Store:
     def __init__(self):
         self.fields = {}
-        self.users = []
+        self.users = {}
+
+        self.fieldsPatch = {}
+        self.usersPatch = {}
+
+        self.principal = ""
+
+    def begin_transaction(self):
+        self.fieldsPatch = {}
+        self.usersPatch = []
+
+    def set_principal(self, username):
+        self.principal = username
+
+    def create_principal(self, username, password):
+        if this.principal == "admin":
+            if !self._user_exists(username):
+                self.usersPatch[username] = password
+            else:
+                raise RuntimeError("FAILED")
+        else:
+            raise RuntimeError("DENIED")
+
+    def change_password(self, username, password):
+        if self.principal == "admin" or self.principal == username:
+            if self._user_exists(username):
+                self.usersPatch[username] = password
+            else:
+                raise RuntimeError("FAILED")
+        else:
+            raise RuntimeError("DENIED")
 
     def has_permission(self, username, label, transactionType):
         label = label.split()[0]
-        userId = -1
-        for index, user in enumerate(self.users):
-            if user.name == username:
-                userId = index
-                break
 
-        return userId in self.fields[label][transactionType]
-
-    def apply_transaction(self, transaction):
-        return
+        return username in self.fields[label][transactionType]
 
     def check_password(self, username, password):
         for user in self.users:
             if user.name == username and user.password == password:
                 return True
-        return False
 
-    def user_exists(self, username):
-        for user in self.users:
-            if user.name == username:
+        for user in self.usersPatch:
+            if user.name == username and user.password == password:
                 return True
-
+            
         return False
 
     def field_exists(self, field):
@@ -44,3 +64,22 @@ class Store:
             return self.fields[labellist[0]]["value"]
         elif len(labellist) == 2:
             return self.fields[labellist[0]]["value"][labellist[1]]
+
+    def _user_exists(self, username):
+        for user in this.usersPatch:
+            if user.name == username:
+                return True
+        
+        for user in this.users:
+            if user.name == username:
+                return True
+
+        return False
+
+    def _field_exists(self, label):
+        tags = label.split('.')
+
+        if len(tags) == 1:
+            return tags[0] in this.fields.keys() or tags[0] in this.fieldsPatch.keys()
+        else:
+            return tags[1] in this.fields[tags[0]]["value"].keys() or tags[1] in this.fieldsPatch[tags[0]]["value"].keys()
