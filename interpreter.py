8dd4a654bbeb1ade6a43d4169115ff0564ad8ccb
nlfox <nlfox@msn.cn>
2016-09-24 23:37:08 +0100

    use getattr() to boost the if elif else case

--- interpreter.py
+++ interpreter.py
@@ -26,68 +26,28 @@ class Interpreter(object):
     def accept(self, parser):
         '''Accepts the parser and interprets provided tokens to perform actions on the store and server'''
 
-        result = ""
+        self.result = ""
+        self.flag = True
         try:
             self._auth(parser)
-
             expected_line = 2
-            while True:
+            while self.flag:
                 token = parser.expect("COMMAND")
                 if token.lineno != expected_line:
                     raise RuntimeError("FAILED")
                 expected_line = expected_line + 1
 
                 cmd = token.value
-                if cmd == "set":
-                    self._set(parser)
-                    result = result + self._status_json("SET")
-                elif cmd == "create principal":
-                    self._create_principal(parser)
-                    result = result + self._status_json("CREATE_PRINCIPAL")
-                elif cmd == "set delegation":
-                    self._set_delegation(parser)
-                    result = result + self._status_json("SET_DELEGATION")
-                elif cmd == "change password":
-                    self._change_password(parser)
-                    result = result + self._status_json("CHANGE_PASSWORD")
-                elif cmd == "append to":
-                    self._append(parser)
-                    result = result + self._status_json("APPEND")
-                elif cmd == "local":
-                    self._local(parser)
-                    result = result + self._status_json("LOCAL")
-                elif cmd == "foreach":
-                    self._foreach(parser)
-                    result = result + self._status_json("FOREACH")
-                elif cmd == "delete delegation":
-                    self._delete_delegation(parser)
-                    result = result + self._status_json("DELETE_DELEGATION")
-                elif cmd == "default delegator":
-                    self._default_delegator(parser)
-                    result = result + self._status_json("DEFAULT_DELEGATOR")
-                elif cmd == "exit":
-                    parser.expect("TERMINATOR")
-                    for operation in self.operation_queue:
-                        operation()
-                    result = result + self._status_json("EXITING")
-                    self.controller.end_transaction_exit(result)
-                    break
-                elif cmd == "return":
-                    return_value = parser.expect(*EXPR)
-                    parser.expect("TERMINATOR")
-                    for operation in self.operation_queue:
-                        operation()
-                    value = self.controller.get_value(return_value)
-                    result = result + '{"status":"RETURNING", "output":' + json.dumps(value) + '}'
-                    self.controller.end_transaction(result)
-                    break
-                else:
+                try:
+                    # dispatch method to each func
+                    getattr(self, "_" + "_".join(cmd.split(" ")))(parser)
+                except AttributeError:
                     raise ValueError("Unsupported command was provided")
 
         except RuntimeError as err:
             self.controller.return_error(err.args[0])
             pass
-        return result
+        return self.result
 
     def _parse_expr(self, parser):
         token = parser.expect("ID", "ID_GROUP", "STRING", "LCURLYPAREN")
@@ -113,7 +73,6 @@ class Interpreter(object):
         parser.expect("PASSWORD")
         password = parser.expect("STRING").value
         parser.expect("DO")
-
         self.controller.begin_transaction(username, password)
 
     def _set(self, parser):
@@ -121,28 +80,33 @@ class Interpreter(object):
         parser.expect("EQUAL")
         expr = self._parse_expr(parser)
         self.operation_queue.append(lambda: self.controller.set(variable, expr))
+        self.result += self._status_json("SET")
 
     def _change_password(self, parser):
         name = parser.expect("ID").value
         new_pass = parser.expect("STRING").value
         self.operation_queue.append(lambda: self.controller.change_password(name, new_pass))
+        self.result += self._status_json("CHANGE_PASSWORD")
 
     def _create_principal(self, parser):
         name = parser.expect("ID").value
         new_pass = parser.expect("STRING").value
         self.operation_queue.append(lambda: self.controller.create_principal(name, new_pass))
+        self.result += self._status_json("CREATE_PRINCIPAL")
 
-    def _append(self, parser):
+    def _append_to(self, parser):
         val = parser.expect("ID").value
         parser.expect("WITH")
         expr = self._parse_expr(parser)
         self.operation_queue.append(lambda: self.controller.append(val, expr))
+        self.result += self._status_json("APPEND")
 
     def _local(self, parser):
         variable = parser.expect("ID").value
         parser.expect("EQUAL")
         expr = self._parse_expr(parser)
         self.operation_queue.append(lambda: self.controller.local(variable, expr))
+        self.result += self._status_json("LOCAL")
 
     def _foreach(self, parser):
         y = parser.expect("ID").value
@@ -151,6 +115,7 @@ class Interpreter(object):
         parser.expect("REPLACEWITH")
         expr = self._parse_expr(parser)
         self.operation_queue.append(lambda: self.controller.foreach(y, x, expr))
+        self.result += self._status_json("FOREACH")
 
     def _set_delegation(self, parser):
         tgt = parser.expect("ID", "ALL")
@@ -159,6 +124,7 @@ class Interpreter(object):
         parser.expect("ARROW")
         p = parser.expect("ID").value
         self.operation_queue.append(lambda: self.controller.set_delegation(tgt, q, right, p))
+        self.result += self._status_json("SET_DELEGATION")
 
     def _delete_delegation(self, parser):
         tgt = parser.expect("ID", "ALL")
@@ -167,8 +133,28 @@ class Interpreter(object):
         parser.expect("ARROW")
         p = parser.expect("ID").value
         self.operation_queue.append(lambda: self.controller.delete_delegation(tgt, q, right, p))
+        self.result += self._status_json("DELETE_DELEGATION")
 
     def _default_delegator(self, parser):
         parser.expect("EQUAL")
         user = parser.expect("ID").value
         self.operation_queue.append(lambda: self.controller.default_delegator(user))
+        self.result += self._status_json("DEFAULT_DELEGATOR")
+
+    def _exit(self, parser):
+        parser.expect("TERMINATOR")
+        for operation in self.operation_queue:
+            operation()
+        result = self.result + self._status_json("EXITING")
+        self.controller.end_transaction_exit(result)
+        self.flag = False
+
+    def _return(self, parser):
+        return_value = parser.expect(*EXPR)
+        parser.expect("TERMINATOR")
+        for operation in self.operation_queue:
+            operation()
+        value = self.controller.get_value(return_value)
+        self.result += '{"status":"RETURNING", "output":' + json.dumps(value) + '}'
+        self.controller.end_transaction(self.result)
+        self.flag = False
