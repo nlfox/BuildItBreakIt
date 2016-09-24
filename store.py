3dbfe543a14a9819e3a1cacd46a0a80f222692a2
nlfox <nlfox@msn.cn>
2016-09-24 09:54:53 +0100

    python style grammar fix

--- store.py
+++ store.py
@@ -1,49 +1,48 @@
 #!/usr/bin/python2
 
 class Store:
+    def __init__(self):
+        self.fields = {}
+        self.users = []
 
-    def init():
-        this.fields = {}
-        this.users = []
-    
-    def has_permission(username, label, transactionType):
+    def has_permission(self, username, label, transactionType):
         userId = -1
-        for index, user in enumerate(this.users):
+        for index, user in enumerate(self.users):
             if user.name == username:
                 userId = index
                 break
-        
-        return userId in this.fields[label][transactionType]
 
-    def apply_transaction(transaction):
+        return userId in self.fields[label][transactionType]
+
+    def apply_transaction(self, transaction):
         return
 
-    def check_password(user, password):
-        for password, user in enumerate(this.users):
+    def check_password(self, user, password):
+        for password, user in enumerate(self.users):
             if user.password == password:
                 return True
         return False
 
-    def user_exists(username):
-        for user in users:
+    def user_exists(self, username):
+        for user in self.users:
             if user.name == username:
                 return True
 
         return False
 
-    def field_exists(field):
+    def field_exists(self, field):
         tags = field.split('.')
-        field = this.fields
+        field = self.fields
         for tag in tags:
             if tag not in field.keys():
                 return False
-        
+
         return True
 
-    def get_field(label):
+    def get_field(self, label):
         def get_field(label):
             labellist = label.split('.')
             if len(labellist == 1):
-                return this.fields[labellist[0]]["value"] 
+                return self.fields[labellist[0]]["value"]
             elif len(labellist == 2):
-                return this.fields[labellist[0]]["value"][labellist[1]]
+                return self.fields[labellist[0]]["value"][labellist[1]]
