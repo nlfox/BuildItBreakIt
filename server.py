1d24c120583d0134bc55c467998ad30b6d29e64b
James Tautges <jamesdtautges@gmail.com>
2016-09-30 17:23:46 +0100

    Add SIGTERM and error code handling

--- server.py
+++ server.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python2
 
 import socket
+import sys
+import signal
 from parser import Lexer
 
 class Server(object):
     def __init__(self, hostname, port):
         self.address = (hostname, port)
         self.run = True
+        self.connection = None
+        signal.signal(signal.SIGTERM, self.end)
 
     def start(self, interpreter):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind(self.address)
+        try:
+            self.sock.bind(self.address)
+        except socket.error:
+            sys.exit(63)
         self.sock.listen(1)
         while self.run:
-            self.connection, client_address = self.sock.accept()    
+            try:
+                self.connection, client_address = self.sock.accept()
+            except socket.error:
+                self.end()
+                return
+
             try:
                 result = ""
                 while True:
@@ -23,4 +35,10 @@ class Server(object):
                         break
                 self.connection.sendall(interpreter.accept(result.decode('string-escape')))
             finally:
-                self.connection.close()
+                if self.connection:
+                    self.connection.close()
+
+    def end(self, signum=15, frame=None):
+        self.run = False
+        self.sock.close()
+        sys.exit(0)
