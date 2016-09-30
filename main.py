9e249341e0281d4028c19c3719b92ea2b26d4ade
Matthew Dupraz <matthew.dupraz@gmail.com>
2016-09-30 21:56:43 +0000

    Formalities

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python2
 
+import sys
+import re
 from store import Store
 from controller import Controller
 from server import Server
@@ -9,13 +11,25 @@ from argparse import ArgumentParser
 
 def main():
     argparser = ArgumentParser()
-    argparser.add_argument("port", help="Port number to be used by the server", type=int)
+    argparser.add_argument("port", help="Port number to be used by the server", type=str)
     argparser.add_argument("password", help="Password to be used for the admin account", nargs="?", default="admin",
                            type=str)
     args = argparser.parse_args()
 
+    if len(args.password) > 4096 or len(args.port) > 4096:
+        sys.exit(255)
+    if not args.port.isdigit() or args.port[0] == "0":
+        sys.exit(255)
+
+    pattern = re.compile("^[A-Za-z0-9_ ,;.?!-]*$")
+    if not pattern.match(args.password):
+        sys.exit(255)
+
     hostname = "localhost"
-    port = args.port
+    port = int(args.port)
+
+    if port < 1024 or port > 65535:
+        sys.exit(0)
 
     store = Store(args.password)
 
--- server.py
+++ server.py
@@ -10,35 +10,48 @@ class Server(object):
         self.address = (hostname, port)
         self.run = True
         self.connection = None
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         signal.signal(signal.SIGTERM, self.end)
 
     def start(self, interpreter):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             self.sock.bind(self.address)
         except socket.error:
             sys.exit(63)
         self.sock.listen(1)
         while self.run:
+            # Initiate connection
+            self.sock.settimeout(None)
             try:
                 self.connection, client_address = self.sock.accept()
             except socket.error:
-                self.end()
-                return
+                continue
 
+            # Accept data
+            self.sock.settimeout(30.0)
             try:
                 result = ""
                 while True:
                     data = self.connection.recv(4096)
-                    result = result + data
+                    result += data
                     if "***" in data:
                         break
+            except socket.timeout:
+                self.connection.sendall('{"status":"TIMEOUT"}')
+                self.connection.close()
+                continue
+
+            # Execute commands
+            try:
                 self.connection.sendall(interpreter.accept(result.decode('string-escape')))
+            except:
+                self.end()
             finally:
                 if self.connection:
                     self.connection.close()
 
     def end(self, signum=15, frame=None):
         self.run = False
-        self.sock.close()
+        if self.sock:
+            self.sock.close()
         sys.exit(0)
