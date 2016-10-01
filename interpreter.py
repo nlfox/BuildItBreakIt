081aa716c02e5d0c212a211f900dfcf7cb3a2668
James Tautges <jamesdtautges@gmail.com>
2016-10-01 15:26:16 +0100

    Concatenate strings with join because that's where the bottleneck is now?

--- interpreter.py
+++ interpreter.py
@@ -34,8 +34,8 @@ class Interpreter(object):
         """Accepts the parser and interprets provided tokens to perform actions on the store and server"""
         if len(data) > 1000000:
             return _status_json("FAILED")
-        
-        self.result = ""
+
+        self.result = []
         self.flag = True
         parser = self.parser.setNewData(data)
         try:
@@ -53,7 +53,7 @@ class Interpreter(object):
 #            traceback.print_exc()
             self.operation_queue = []
             return _status_json(err.args[0])
-        return self.result
+        return ''.join(self.result)
 
     def _parse_expr(self):
         token = self.parser.expect("ID", "ID_GROUP", "STRING", "LCURLYPAREN", "SQUBRACKETS")
@@ -91,33 +91,33 @@ class Interpreter(object):
         self.parser.expect("EQUAL")
         expr = self._parse_expr()
         self.operation_queue.append(lambda: self.controller.set(variable, expr))
-        self.result += _status_json("SET")
+        self.result.append(_status_json("SET"))
 
     def _change_password(self):
         name = self.parser.expect("ID").value
         new_pass = self.parser.expect("STRING").value
         self.operation_queue.append(lambda: self.controller.change_password(name, new_pass))
-        self.result += _status_json("CHANGE_PASSWORD")
+        self.result.append(_status_json("CHANGE_PASSWORD"))
 
     def _create_principal(self):
         name = self.parser.expect("ID").value
         new_pass = self.parser.expect("STRING").value
         self.operation_queue.append(lambda: self.controller.create_principal(name, new_pass))
-        self.result += _status_json("CREATE_PRINCIPAL")
+        self.result.append(_status_json("CREATE_PRINCIPAL"))
 
     def _append_to(self):
         val = self.parser.expect("ID").value
         self.parser.expect("WITH")
         expr = self._parse_expr()
         self.operation_queue.append(lambda: self.controller.append_to(val, expr))
-        self.result += _status_json("APPEND")
+        self.result.append(_status_json("APPEND"))
 
     def _local(self):
         variable = self.parser.expect("ID").value
         self.parser.expect("EQUAL")
         expr = self._parse_expr()
         self.operation_queue.append(lambda: self.controller.local(variable, expr))
-        self.result += _status_json("LOCAL")
+        self.result.append(_status_json("LOCAL"))
 
     def _foreach(self):
         y = self.parser.expect("ID").value
@@ -126,7 +126,7 @@ class Interpreter(object):
         self.parser.expect("REPLACEWITH")
         expr = self._parse_expr()
         self.operation_queue.append(lambda: self.controller.foreach(y, x, expr))
-        self.result += _status_json("FOREACH")
+        self.result.append(_status_json("FOREACH"))
 
     def _set_delegation(self):
         tgt = self.parser.expect("ID", "ALL").value
@@ -135,7 +135,8 @@ class Interpreter(object):
         self.parser.expect("ARROW")
         p = self.parser.expect("ID").value
         self.operation_queue.append(lambda: self.controller.set_delegation(tgt, q, right, p))
-        self.result += _status_json("SET_DELEGATION")
+        s = _status_json("SET_DELEGATION")
+        self.result.append(s)
 
     def _delete_delegation(self):
         tgt = self.parser.expect("ID", "ALL").value
@@ -144,19 +145,19 @@ class Interpreter(object):
         self.parser.expect("ARROW")
         p = self.parser.expect("ID").value
         self.operation_queue.append(lambda: self.controller.delete_delegation(tgt, q, right, p))
-        self.result += _status_json("DELETE_DELEGATION")
+        self.result.append(_status_json("DELETE_DELEGATION"))
 
     def _default_delegator(self):
         self.parser.expect("EQUAL")
         user = self.parser.expect("ID").value
         self.operation_queue.append(lambda: self.controller.default_delegator(user))
-        self.result += _status_json("DEFAULT_DELEGATOR")
+        self.result.append(_status_json("DEFAULT_DELEGATOR"))
 
     def _exit(self):
         self.parser.expect("NEWLINE")
         self.parser.expect("TERMINATOR")
         self._execute_operations()
-        self.result += _status_json("EXITING")
+        self.result.append(_status_json("EXITING"))
         self.controller.end_transaction_exit()
         self.flag = False
 
@@ -166,10 +167,10 @@ class Interpreter(object):
         self.parser.expect("TERMINATOR")
         self._execute_operations()
         value = self.controller.get_value(return_value)
-        self.result += '{"status":"RETURNING", "output":' + json.dumps(value) + '}'
+        self.result.append('{"status":"RETURNING", "output":%s}' % (json.dumps(value)))
         self.controller.end_transaction()
         self.flag = False
 
 
 def _status_json(status):
-    return '{"status":"' + status + '"}\n'
+    return '{"status":"%s"}\n' % (status)
